1729afa97c7ad2f98f10c3c567c329cceb51e405
TanyaMarchenkoS <marcenkot466@gmail.com>
2022-09-14 20:09:24 +0300

    2 завдання зроблено

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.querySelector('#ingredients');
+console.log(listRef);
+
+const listItems = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+
+  return itemEl;
+});
+
+listRef.append(...listItems);
