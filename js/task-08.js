7b5c404920d596084c31af54d1dcd7e2eda1c0bf
TanyaMarchenkoS <marcenkot466@gmail.com>
2022-09-20 23:03:12 +0300

    завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, pessword }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        alert('Заполнены не все поля, пожалуйста заполните');
+    };
+
+    const obj = { Email: email.value, Password: password.value };
+
+    console.log(obj);
+    form.reset();
+});
