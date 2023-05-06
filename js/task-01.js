b0008085491f27f210136962b90420c6f6bbfcff
TanyaMarchenkoS <marcenkot466@gmail.com>
2023-05-06 23:49:34 +0300

    зроблене 1 завдання

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
-const categories = document.querySelectorAll('.item');
+const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((item) => {
     const titleCategories = item.querySelector('h2');
-    const itemList = item.querySelectorAll('li');
     console.log(`Category: ${titleCategories.textContent}`);
+    const itemList = item.querySelectorAll('li');
     console.log(`Elements: ${itemList.length}`);
 });
